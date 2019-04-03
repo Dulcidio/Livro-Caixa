10b8d8e65de16c3ccc0132411d52b1ac454ec558
Dulcidio da Luz Sobrinho <dulcidio.sobrinho@gmail.com>
2019-04-03 12:38:56 -0300

    Enviando projeto

---
+++ src/main/java/br/com/loftsistemas/caixabanco/dao/FuncionarioDao.java
@@ -0,0 +1,20 @@
+/**
+ * @author Dulcidio Sobrinho
+ * 16/01/2019
+ */
+package br.com.loftsistemas.caixabanco.dao;
+
+  
+
+import br.com.loftsistemas.caixabanco.dominio.Funcionario;
+
+ 
+
+/**
+ * @author Dulcidio Sobrinho
+ *
+ */
+public class FuncionarioDao  extends GenericDAO<Funcionario>{
+
+	 
+}
